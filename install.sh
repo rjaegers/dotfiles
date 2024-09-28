81c63a832d5f774f1473d0a49161145a0c33fef6
Ron <45816308+rjaegers@users.noreply.github.com>
2024-09-28 11:32:13 +0200

    fix: quote Starship install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Install Starship (https://starship.rs) shell
-sh -c $(wget -qO- https://starship.rs/install.sh) -y -f > /dev/null
+sh -c "$(wget -qO- https://starship.rs/install.sh)" -y -f > /dev/null
 grep -qxF 'eval "$(starship init bash)"' ~/.bashrc || echo 'eval "$(starship init bash)"' >> ~/.bashrc
