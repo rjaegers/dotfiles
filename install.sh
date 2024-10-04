2f9048e591864eb02de31c1c90203bf099c77fe0
rjaegers <ron.jaegers@philips.com>
2024-10-04 15:48:37 +0200

    fix: work around missing git in eza aarch64

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ grep -qxF 'eval "$(starship init bash)"' ~/.bashrc || echo 'eval "$(starship ini
 ln -sf "$(pwd)/dotfiles/.config/starship.toml" ~/.config/starship.toml
 
 # Install eza (https://eza.rocks) a modern alternative to ls
-wget -qO- https://github.com/eza-community/eza/releases/latest/download/eza_$(uname -m)-unknown-linux-gnu.tar.gz | tar -xzv -C /usr/local/bin
+if [ "$(uname -m)" = "aarch64" ]; then
+    # Work around https://github.com/eza-community/eza/issues/1060
+    wget -qO- https://github.com/user-attachments/files/16258190/eza_aarch64-unknown-linux-gnu.tar.gz | tar -xzv -C /usr/local/bin
+else
+    wget -qO- https://github.com/eza-community/eza/releases/latest/download/eza_$(uname -m)-unknown-linux-gnu.tar.gz | tar -xzv -C /usr/local/bin
+fi
 
 git clone https://github.com/eza-community/eza-themes.git > /dev/null
 mkdir -p ~/.config/eza
